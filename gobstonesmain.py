ef2fe794098e419c29fd4198930be3d1dc9fe323
Jieun Lim <123513492+LiaJ2003@users.noreply.github.com>
2023-11-21 21:28:32 -0500

    Add files via upload

---
+++ gobstonesmain.py
@@ -0,0 +1,7 @@
+from cmu_graphics import *
+from marbles import Marbles
+
+marble = Marbles(5, 5, 10, "red")
+print(marble.x)
+
+
---
+++ marbles.py
@@ -0,0 +1,11 @@
+class Marbles():
+    def __init__(self, x, y, radius, team):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.team = team
+        self.collide = False
+
+    def isCollision(self):
+        return
+    
