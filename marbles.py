a23c677ff7caca9f9aa472ea40dc808c3ebd0f73
LiaJ2003 <jieunlim@andrew.cmu.edu>
2023-12-20 21:58:11 -0800

    Update marbles.py

--- marbles.py
+++ marbles.py
@@ -4,8 +4,14 @@ class Marbles():
         self.y = y
         self.radius = radius
         self.team = team
+        self.energy = 0
+        self.speed = 0
         self.collide = False
 
     def isCollision(self):
+        #might need to do other
         return
     
+    def getNewSpeed(self):
+        #have set acceleration
+        return
