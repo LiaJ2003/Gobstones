c88c1112361a7c45c7cbc8e3dc0bc60b8571e4c7
LiaJ2003 <jieunlim@andrew.cmu.edu>
2023-12-26 12:07:33 -0800

    added player class and edited main

--- gobstonesmain.py
+++ gobstonesmain.py
@@ -1,5 +1,5 @@
 from cmu_graphics import *
-from marbles import Marbles
+from marbles import Marble
 from button import Button
 
 def onAppStart(app):
@@ -9,6 +9,37 @@ def onAppStart(app):
     app.help = False
     app.settings = False
     buttons(app)
+    players(app)
+    marbles(app)
+
+def players(app):
+    #player 1
+    app.player1Name = "Player 1"
+    app.player1C = "red"
+
+    #player 2
+    app.player2Name = "Player 2"
+    app.player2C = "blue"
+
+def marbles(app):
+    #player 1 marbles
+    app.p1m1 = Marble(60, 120, 25, app.player1Name)
+    app.p1m2 = Marble(60, 230, 25, app.player1Name)
+    app.p1m3 = Marble(60, 340, 25, app.player1Name)
+    app.p1m4 = Marble(60, 450, 25, app.player1Name)
+
+    app.player1M = [app.p1m1, app.p1m2, app.p1m3, app.p1m4]
+    
+    #player 2 marbles
+    app.p2m1 = Marble(app.width-110, 120, 25, app.player2Name)
+    app.p2m2 = Marble(app.width-110, 230, 25, app.player2Name)
+    app.p2m3 = Marble(app.width-110, 340, 25, app.player2Name)
+    app.p2m4 = Marble(app.width-110, 450, 25, app.player2Name)
+
+    app.player2M = [app.p2m1, app.p2m2, app.p2m3, app.p2m4]
+
+    #total marbles
+    app.marbles = app.player1M + app.player2M
 
 def buttons(app):
     app.beginB = Button(550, 200, 500, 100)
@@ -17,6 +48,7 @@ def buttons(app):
     app.settingsB = Button(550, 650, 500, 100)
 
 def onMousePress(app, mouseX, mouseY):
+    #initial game settings
     if app.beginB.isClicked(mouseX, mouseY):
         app.start = True
     if app.contB.isClicked(mouseX, mouseY) and app.contPoss:
@@ -25,6 +57,15 @@ def onMousePress(app, mouseX, mouseY):
         app.help = True
     if app.settingsB.isClicked(mouseX, mouseY):
         app.settings = True
+
+    #when in game mode, checking to see if marble is being moved
+    for marble in app.marbles:
+        if marble.isChosen(mouseX, mouseY):
+            marble.chosen = True
+
+def onMouseDrag(app, mouseX, mouseY):
+
+    return
 
 def onKeyPress(app, key):
     if key == "r":
@@ -56,18 +97,31 @@ def drawStartScreen(app):
               app.settingsB.y + app.settingsB.height//2, size=24, font='cinzel',
               fill = "white")
 
-def drawGame(app):
+def drawSnakeGame(app):
     drawCircle(app.width//2, app.height//2, 300, fill = None, border = "black")
     drawCircle(app.width//2, app.height//2, 200, fill = None, border = "black")
     drawCircle(app.width//2, app.height//2, 100, fill = None, border = "black")
+
+def drawMarbles(app):
+    drawLabel(app.player1Name, 160, 60, size=30, font="cinzel")
+    for marble in app.player1M:
+        drawCircle(marble.x, marble.y, marble.radius, fill = app.player1C)
+    
+    drawLabel(app.player2Name, app.width-220, 60, size=30, font="cinzel")
+    for marble in app.player2M:
+        drawCircle(marble.x, marble.y, marble.radius, fill = app.player2C)
+
 
 def redrawAll(app):
     if not app.start:
         drawStartScreen(app)
     if app.start:
-        drawGame(app)
+        drawSnakeGame(app)
+        drawMarbles(app)
 
-marble = Marbles(5, 5, 10, "red")
-print(marble.x)
+# def onStep(app):
+#     for marble in app.movingmarbles:
+#         marble
+
 
 runApp(width = 1600, height = 800)
--- marbles.py
+++ marbles.py
@@ -1,4 +1,4 @@
-class Marbles():
+class Marble():
     def __init__(self, x, y, radius, team):
         self.x = x
         self.y = y
@@ -6,12 +6,15 @@ class Marbles():
         self.team = team
         self.energy = 0
         self.speed = 0
+        self.chosen = False
         self.collide = False
 
-    def isCollision(self):
-        #might need to do other
-        return
+    def isChosen(self, mX, mY):
+        if mX <= self.x+self.radius and mX >= self.x-self.radius:
+            if mY <= self.y+self.radius and mY >= self.y-self.radius:
+                return True
+        return False
     
     def getNewSpeed(self):
         #have set acceleration
-        return
+        return self.speed
