9c7a7036d038f59007c8bf1eeb7505902270b2d0
clairekimmie <clairek3@andrew.cmu.edu>
2023-12-27 19:03:34 -0800

    move and confirm marble location

--- marbles.py
+++ marbles.py
@@ -1,3 +1,4 @@
+from buttons import Button
 class Marble():
     def __init__(self, x, y, radius, team):
         self.x = x
@@ -8,6 +9,9 @@ class Marble():
         self.speed = 0
         self.chosen = False
         self.collide = False
+        self.dragging = False
+        self.confirm = False #to confirm location
+        self.confirmButton = Button(x, y, 60, 30)
 
     def isChosen(self, mX, mY):
         if mX <= self.x+self.radius and mX >= self.x-self.radius:
@@ -18,3 +22,22 @@ class Marble():
     def getNewSpeed(self):
         #have set acceleration
         return self.speed
+
+    def updatePosition(self, mouseX, mouseY, app):
+        if self.dragging:
+            new_x = max(app.boardLeft + self.radius,
+                        min(mouseX, app.boardLeft + app.boardWidth - self.radius))
+            new_y = max(app.boardTop + self.radius,
+                        min(mouseY, app.boardTop + app.boardHeight - self.radius))
+            
+            innerBoardLeft = (app.width - app.boardWidth + 4 * self.radius) // 2
+            innerBoardTop = (app.height - app.boardHeight + 4 * self.radius) // 2
+            innerBoardRight = innerBoardLeft + app.boardWidth - 4 * self.radius
+            innerBoardBottom = innerBoardTop + app.boardHeight - 4 * self.radius
+
+            if innerBoardLeft <= new_x <= innerBoardRight and innerBoardTop <= new_y <= innerBoardBottom:
+                #no updating poition if its insider the inner board
+                return
+
+            self.x = new_x
+            self.y = new_y
