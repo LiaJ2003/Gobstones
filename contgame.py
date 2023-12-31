154d1081c4c176d4c77b442d6916e927251d0635
clairekimmie <151698772+clairekimmie@users.noreply.github.com>
2023-12-31 00:11:53 -0800

    Delete contgame.py

--- contgame.py
+++
@@ -1,9 +0,0 @@
-#load continued game
-
-#hold the information in csv file; check documentation on prior sem
-
-#should read from csv file and load the information onto the screen
-
-
-#READ: this should be one of the final steps, so pls ignore until other parts
-#are finished - Jieun
